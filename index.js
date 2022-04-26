0dfb4ddfaaa3518098902c29aab03f47069cf98f
Samuel Mandel <slman.760@gmail.com>
2022-04-25 23:08:17 -0700

    cards deploy onto HTML

--- index.js
+++ index.js
@@ -139,9 +139,105 @@ inquirer
 begin();
 
 function renderHTML(){
-    console.log(teamM[0].name) //name
-    console.log(teamI[0].name)
-    console.log(teamE[0].name)
-//fs.writeFile('index.html',htmlSkeleton,(err) =>
-           // err ? console.error(err) : console.log('index.html created!'))
+    // console.log(teamM[0]) //manager object
+    // console.log(teamM[0].name) //name
+    // console.log(teamI[0].name)
+    // console.log(teamE[0].name)
+
+    var cardSkeletonM =
+    `
+    <div class= "col-sm">
+    <div class="card" style="width: 18rem;">
+      <div class="card-header">
+        Manager
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${teamM[0].name}  </li>
+        <li class="list-group-item">${teamM[0].id} </li>
+        <li class="list-group-item">${teamM[0].email}  </li>
+        <li class="list-group-item">${teamM[0].officeNumber} </li>
+      </ul>
+    </div>
+    </div>
+    
+    `
+    for(var i = 0; i<teamI.length; i++){
+        var cardSkeletonI = 
+        `
+<div class= "col-sm">
+<div class="card" style="width: 18rem;">
+<div class="card-header">
+    Intern
+</div>
+<ul class="list-group list-group-flush">
+    <li class="list-group-item">${teamI[i].name}  </li>
+    <li class="list-group-item">${teamI[i].id} </li>
+    <li class="list-group-item">${teamI[i].email}  </li>
+    <li class="list-group-item">${teamI[i].school} </li>
+    
+</ul>
+</div> 
+</div>
+        `
+    }
+    for(var i = 0; i<teamE.length; i++){
+        var cardSkeletonE = 
+        `
+<div class= "col-sm">
+<div class="card" style="width: 18rem;">
+<div class="card-header">
+    Engineer
+</div>
+<ul class="list-group list-group-flush">
+    <li class="list-group-item">${teamE[i].name}  </li>
+    <li class="list-group-item">${teamE[i].id} </li>
+    <li class="list-group-item">${teamE[i].email}  </li>
+    <li class="list-group-item"><a href="https://github.com/${teamE[i].github}">${teamE[i].github}</a></li>
+</ul>
+</div> 
+</div>     
+        `
+    }
+    var HTMLSkeleton = 
+    `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Team Manager</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+</head>
+<body>
+<header>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Team Manager</h1>
+    <p class="lead">See your team below!</p>
+  </div>
+</div></header>
+<main class = "container-fluid">
+<div class = "row align-items-start">
+${cardSkeletonM}
+${cardSkeletonE}
+${cardSkeletonI}
+</div>
+</main> 
+
+
+  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="/index.js"></script>
+</body>
+</html>
+    `
+
+    fs.writeFile('index.html', HTMLSkeleton ,(err) =>
+    err ? console.error(err) : console.log('card added!'))
+    
+    
 }
